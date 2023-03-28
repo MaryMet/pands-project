8db47bd7bcdb7bc8d240cd69716de1cb323d7876
Mary Metcalfe <G00425666@atu.ie>
2023-03-28 20:03:25 +0100

    \

--- analysis.py
+++ analysis.py
@@ -1,3 +1,12 @@
 # Pands Project
 # Iris Fisher data set
 # Author : MAry Metcalfe
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+iris = read_csv("iris.csv")
+
+print (iris.shape)
