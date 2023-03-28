19888ad7fa08cd89f77521d149c2cd10a51bacb1
Mary Metcalfe <G00425666@atu.ie>
2023-03-28 20:15:15 +0100

    \

--- analysis.py
+++ analysis.py
@@ -7,6 +7,8 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 
-iris = read_csv("iris.csv")
+iris =pd.read_csv("iris_csv.csv") # this uses the imported iris_cv dataset
 
-print (iris.shape)
+print (iris.shape) # this prints out the number of data points and the number of columns across
+
+print (iris.columns) # this prints out the titles of the columns
