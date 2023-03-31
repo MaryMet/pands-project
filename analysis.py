42d7ec81074a0729bfbb430e23c6956d45158c79
Mary Metcalfe <G00425666@atu.ie>
2023-03-31 19:30:56 +0100

    \

--- analysis.py
+++ analysis.py
@@ -17,9 +17,29 @@ print (iris["class"].value_counts()) # this prints out the class/species of the 
 
 print (iris.head(10)) # print out the first ten rows of the dataset
 
+iris['class'].unique() # find out unique classification 
+print(iris.groupby('class').size()) # print out above the uniques classification of the iris flowers and the amount of each.
+
+# printing out a number of mathematical principals - 
+print (iris.min()) # prints out the minimum measurement of the dataset
+print (iris.max()) # prints out the maximum measurement of the dataset
+print (iris.mean()) # prints out the mean/average of the measurements/dataset
+print (iris.median()) # prints out the medium value of the measurements/dataset
+print (iris.std()) # prints of the standard deviation
+
+# creating a table which illustates the above mathematical principals above in the form of a table
+
+summary = iris.describe()
+summary = summary.transpose()
+print (summary.head ())
+
+# basic scatter plot
 iris.plot(kind = "scatter", x = "sepallength", y = "petallength", color = "red") # visualising some of the data in a scattergraph
 
 plt.title ("sepal vs petal", color = "green")  # title of the scattergraph
+
+# basic boxplot to visualise one or more groups of numerical data. It compares the distributions of the Sepal Length, Sepal Width, Petal Length and Petal Width
+
 
 
 
